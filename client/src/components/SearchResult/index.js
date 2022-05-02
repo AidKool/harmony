7f64021c9fee532b237e4679247963f0b44f832d
jordi <jordi.lanau@yahoo.com>
2022-05-02 10:35:41 +0100

    #58 Style SearchResult component

--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineCheck, AiOutlineClose } from 'react-icons/ai';
 
 import capitalise from '../../utils/capitalise';
 
@@ -15,7 +16,7 @@ function SearchResult(props) {
 
   return (
     <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-5">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center relative">
         <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src={picture} alt={username} />
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{username}</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">{type}</span>
@@ -23,19 +24,35 @@ function SearchResult(props) {
         <p>Distance: {miles} miles</p>
         Genres:
         <ul>
-          {genres.map((genre) => {
-            return <li key={genre}>{genre}</li>;
+          {genres.map((genre, index, array) => {
+            return (
+              <li key={genre} className="text-sm inline">
+                {capitalise(genre)}
+                {index < array.length - 1 ? ', ' : ''}
+              </li>
+            );
           })}
         </ul>
         {type === 'Musician' ? (
           <>
             Instruments:
             <ul>
-              {instruments.map((instrument) => {
-                return <li key={instrument}>{instrument}</li>;
+              {instruments.map((instrument, index, array) => {
+                return (
+                  <li key={instrument} className="text-sm inline">
+                    {capitalise(instrument)}
+                    {index < array.length - 1 ? ', ' : ''}
+                  </li>
+                );
               })}
             </ul>
-            <p>{available ? 'Ready for a new challenge' : 'Not available'}</p>
+            <div className="icon-container absolute right-0">
+              {available ? (
+                <AiOutlineCheck className="bg-green-500 rounded-full text-xl p-1" />
+              ) : (
+                <AiOutlineClose className="bg-red-500 rounded-full text-xl p-1" />
+              )}
+            </div>
           </>
         ) : (
           ''
@@ -46,11 +63,11 @@ function SearchResult(props) {
             className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             View Profile
           </Link>
-          <a
-            href="#"
+          <Link
+            to={``}
             className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
             Message
-          </a>
+          </Link>
         </div>
       </div>
     </div>
