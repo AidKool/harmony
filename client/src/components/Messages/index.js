6fd0cc89aa3cd4a36eaed303cdd3aeb6bfee244d
jordi <jordi.lanau@yahoo.com>
2022-05-06 20:36:02 +0100

    #81 Add Link to user profile on Messages component

--- client/src/components/Messages/index.js
+++ client/src/components/Messages/index.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useQuery, useLazyQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
 import { useMessageContext } from '../../store/messageContext';
 import { useChatContext } from '../../store/chatContext';
-
 import { GET_ME, GET_CHAT } from '../../utils/queries';
 
 import './messages.css';
@@ -11,18 +11,13 @@ import './messages.css';
 function Messages() {
   const { loading: userLoading, data: userRawData } = useQuery(GET_ME);
   const userData = userRawData?.me || {};
-  console.log('me:', userData);
 
   const { activeChat, setActiveChat } = useChatContext();
 
   const [getChat, { loading, data }] = useLazyQuery(GET_CHAT);
   const chatData = data?.getChat || null;
 
-  // const [contact, setContact] = useState({});
-
   const { contact, setContact } = useMessageContext();
-
-  console.log('chatData:', chatData);
 
   useEffect(() => {
     const getChatData = async () => {
@@ -34,27 +29,26 @@ function Messages() {
     };
 
     if (activeChat) {
-      console.log('active chat:', activeChat);
       getChatData();
 
       if (chatData) {
-        console.log('users:', chatData.users);
         if (chatData.users[0]._id === userData._id) {
           setContact(chatData.users[1]);
         } else {
           setContact(chatData.users[0]);
         }
       }
-      console.log('contact:', contact);
     }
-  }, [activeChat, chatData, contact, getChat, userData._id]);
+  }, [activeChat, chatData, contact, getChat, setContact, userData._id]);
 
   return (
-    <div className="px-5 flex flex-col chat-height">
-      <header className="py-3">
-        <h2>Person 1</h2>
+    <div className="flex flex-col chat-height">
+      <header className="px-5 py-3">
+        <Link to={`/profiles/${contact._id}`} className="font-bold border-b-2 border-white hover:border-blue-600">
+          {contact.username}
+        </Link>
       </header>
-      <div className="flex flex-col gap-y-3 overflow-y-auto h-full justify-end">
+      <div className="flex flex-col gap-y-3 overflow-y-auto h-full justify-end px-5 ">
         {chatData?.messages &&
           chatData.messages.length > 0 &&
           chatData.messages.map((message) => {
